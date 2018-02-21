0b65c9b0c0308f3b2f8dcf96ddc8247bbab507b4
debbie <drosenfeld87@gmail.com>
2018-02-21 15:07:18 -0500

    complete routing-controllers exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
 get '/welcome' => 'pages#welcome'
-get '/' => 'pages#welcome'
-get '/about' => 'pages#about'
-get '/contest' => 'pages#contest'
-get '/kitten/:size' => 'pages#kitten'
-get '/kittens/:size' => 'pages#kittens'
-get '/secrets/:magic_word' => 'pages#secrets'
+root 'pages#welcome'
+get '/about' => 'pages#about', as: 'about'
+get '/contest' => 'pages#contest', as: 'contest'
+get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+get '/kittens/:size' => 'pages#kittens', as: 'kittens'
+get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 
 end
