4c4214399122681134d747bf487d1ad4c61348ac
debbie <drosenfeld87@gmail.com>
2018-02-21 14:36:12 -0500

    kittens and kitten pages are working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
   def welcome
     @header = "Welcome to my website"
@@ -13,8 +14,16 @@ class PagesController < ApplicationController
   end
 
   def kitten
-  requested_size = params[:size]
-  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+
+  end
+
+  def kittens
+
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 
 end
